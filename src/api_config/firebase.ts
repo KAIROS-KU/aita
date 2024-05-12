ff683230af164a89936fb0bdeadecf3f2d3d901d
taykim01 <taykim01@gmail.com>
2024-05-12 21:05:21 +0900

    doc: 파일 세팅

--- src/api_config/firebase.ts
+++ src/api_config/firebase.ts
@@ -4,11 +4,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "ai-ta-206f2.firebaseapp.com",
-  projectId: "ai-ta-206f2",
-  storageBucket: "ai-ta-206f2.appspot.com",
-  messagingSenderId: "928712618229",
-  appId: "1:928712618229:web:db1828a230d5bf0c2f95bc"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
